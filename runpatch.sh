b101f1cb43912ef2939e8f5aaccc92d45a655491
boypt <1033514+boypt@users.noreply.github.com>
2022-10-13 11:18:11 +0800

    add script

---
+++ runpatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mv /boot/{boot.cmd,boot.cmd.orig}
+mv /boot/{boot.scr,boot.scr.orig}
+cp /boot/boot.cmd.orig ./boot.cmd
+patch -p1 < boot.cmd.patch
+mkimage -C none -A arm -T script -d boot.cmd boot.scr
+cp boot.* /boot
+
+mv /boot/dtb/{sun8i-h3-nanopi-neo.dtb,sun8i-h3-nanopi-neo.dtb.orig}
+dtc -I dtb -O dts -o sun8i-h3-nanopi-neo.dts /boot/dtb/sun8i-h3-nanopi-neo.dtb
+[[ $(uname -r) == 5.* ]] && patch -p1 < sun8i-h3-nanopi-neo.5.15.25.patch
+[[ $(uname -r) == 4.* ]] && patch -p1 < sun8i-h3-nanopi-neo.dts.patch
+dtc -I dts -O dtb -o sun8i-h3-nanopi-neo.dtb sun8i-h3-nanopi-neo.dts
+cp sun8i-h3-nanopi-neo.dtb /boot/dtb
