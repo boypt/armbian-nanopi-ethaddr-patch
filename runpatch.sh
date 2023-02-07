a6ee1218df3f3b2b7716348b1ed1bd357c9ddb9a
Preston <1033514+boypt@users.noreply.github.com>
2023-02-08 00:24:12 +0800

    fix runpart orig

--- runpatch.sh
+++ runpatch.sh
@@ -8,7 +8,7 @@ mkimage -C none -A arm -T script -d boot.cmd boot.scr
 cp boot.* /boot
 
 mv /boot/dtb/{sun8i-h3-nanopi-neo.dtb,sun8i-h3-nanopi-neo.dtb.orig}
-dtc -I dtb -O dts -o sun8i-h3-nanopi-neo.dts /boot/dtb/sun8i-h3-nanopi-neo.dtb
+dtc -I dtb -O dts -o sun8i-h3-nanopi-neo.dts /boot/dtb/sun8i-h3-nanopi-neo.dtb.orig
 [[ $(uname -r) == 5.* ]] && patch -p1 < sun8i-h3-nanopi-neo.5.15.25.patch
 [[ $(uname -r) == 4.* ]] && patch -p1 < sun8i-h3-nanopi-neo.dts.patch
 dtc -I dts -O dtb -o sun8i-h3-nanopi-neo.dtb sun8i-h3-nanopi-neo.dts
